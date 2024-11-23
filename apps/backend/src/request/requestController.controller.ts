aaf9fe4406047268aa2e63c2e860d0e521dc98e1
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-23 17:55:45 -0300

    feat: create request controller

--- apps/backend/src/request/requestController.controller.ts
+++ apps/backend/src/request/requestController.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { RequestProvider } from './requestProvider.provider';
 import { IRequest, Request } from '@acolhe/core';
 
@@ -12,7 +12,40 @@ export class RequestController {
   ): Promise<IRequest[]> {
     const request = new Request(this.repo);
     try {
-      return await this.repo.getRequestByUserId(userId);
+      return await request.getRequestByUserId(userId);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Put(':requestId')
+  async updateStatusRequest(
+    @Param('requestId') requestId: number,
+    @Body() status: Partial<IRequest>,
+  ) {
+    const request = new Request(this.repo);
+    try {
+      return await request.updateStatusRequest(requestId, status);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Delete(':requestId')
+  async deleteRequest(@Param('requestId') requestId: number) {
+    const request = new Request(this.repo);
+    try {
+      return await request.deleteRequest(requestId);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Post()
+  async create(@Body() request: IRequest) {
+    const requestProvider = new Request(this.repo);
+    try {
+      return await requestProvider.create(request);
     } catch (error) {
       throw new Error(error.message);
     }

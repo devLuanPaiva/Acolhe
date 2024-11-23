0bf121554b725fddd440c5fec395cff9375686cd
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-23 17:03:54 -0300

    feat: create caregiver controller

---
+++ apps/backend/src/caregiver/caregiverController.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CaregiverProvider } from './caregiverProvider.provider';
+import { Caregiver, ICaregiver } from '@acolhe/core';
+
+@Controller('api/caregiver')
+export class CaregiverController {
+  constructor(private readonly repo: CaregiverProvider) {}
+
+  @Get()
+  async listCaregiver(): Promise<ICaregiver[]> {
+    const caregiver = new Caregiver(this.repo);
+    try {
+      return await caregiver.listCaregiver();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Post('/diary')
+  async addDiary(@Body() value: number, userId: number) {
+    const caregiver = new Caregiver(this.repo);
+    try {
+      return await caregiver.addDiary(value, userId);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Get('/diary')
+  async getCaregiverByDiary(@Body() valueMin: number, valueMax: number) {
+    const caregiver = new Caregiver(this.repo);
+    try {
+      return await caregiver.getCaregiverByDiary(valueMin, valueMax);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  @Post('/evaluate')
+  async evaluateCaregiver(@Body() value: string) {
+    const caregiver = new Caregiver(this.repo);
+    try {
+      return await caregiver.evaluateCaregiver(value);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}

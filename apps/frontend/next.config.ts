dda1dbfd308a87d00e6e99ebe7dd562e5ff1d8f6
Arthur Duarte <tuiduarte@gmail.com>
2024-11-23 10:32:56 -0500

    feat: add hero section

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["medicinasa.com.br"],
+  },
 };
 
 export default nextConfig;

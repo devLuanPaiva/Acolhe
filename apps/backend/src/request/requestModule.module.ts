63a18692fbcef688f67bd6324fe9c671dc294f18
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-24 11:21:24 -0300

    fix: add provider caregiver

--- apps/backend/src/request/requestModule.module.ts
+++ apps/backend/src/request/requestModule.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/db/prisma.module';
 import { RequestController } from './requestController.controller';
 import { RequestProvider } from './requestProvider.provider';
+import { Caregiver } from '@acolhe/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RequestController],
-  providers: [RequestProvider],
+  providers: [RequestProvider, Caregiver],
 })
 export class RequestModule {}
